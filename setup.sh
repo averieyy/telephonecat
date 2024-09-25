0cb4f82ec86652ceaefe71f939776f2df81a57fa
averieyy <averieyy@averieyy.Kuben.it>
2024-09-25 10:37:59 +0200

    refined setup script

--- setup.sh
+++ setup.sh
@@ -3,16 +3,16 @@
 # sudo apt upgrade
 echo 'Installing dependencies'
 sudo apt install postgresql postgresql-client python3 git
-sudo -u postgres 
 sudo useradd phoneboy
 echo 'phoneboy
 phoneboy' | sudo passwd phoneboy
-echo 'CREATE ROLE phoneboy NOSUPERUSER NOCREATEDB NOCREATEROLE INHERIT LOGIN;
+echo "CREATE ROLE phoneboy NOSUPERUSER NOCREATEDB NOCREATEROLE INHERIT LOGIN PASSWORD 'phoneboy';
 CREATE DATABASE telephone;
-GRANT ALL ON DATABASE telephone TO phoneboy;' | sudo -u postgres psql
-sudo -u postgres createdb telephone
+GRANT ALL ON DATABASE telephone TO phoneboy;" | sudo -u postgres psql
+echo 'GRANT ALL ON SCHEMA public TO phoneboy;' | sudo -u postgres psql
 echo '{
   "db": "telephone",
   "user": "phoneboy",
   "password": "phoneboy"
 }' > .secrets.json
+cat db.sql | sudo -u phoneboy psql telephone
